51cf9568239a35b49317e11c2dae15869b489244
Jiachen2cc <zhoujiachen6@gmail.com>
2024-09-18 19:21:13 +0000

    add snapshot loading

--- scene.py
+++ scene.py
@@ -1,15 +1,27 @@
+import json
+from utils import encode_image
 class Scene:
     
     def __init__(self, scene_dir):
         self.scene_dir = scene_dir
-        self.snapshot = ...
+        self.snapshot = {}
         self.gt_bbox = ...
         self.detected_bbox = ...
     
     
     def load_scene(self):
         # get snapshot, gt_bbox, detected_bbox
-        ...
+        snapshot_path = os.path.join(self.scene_dir,"snapshot_inclusive_merged.json")
+        with open(snapshot_path,'r') as f:
+            snapshot_data = json.load(f)
+        for k in snapshot_data.keys():
+            snapshot_img_path = os.path.join(self.scene_dir, results, k)
+            snapshot_img = encode_image(snapshot_img_path)
+            self.snapshot[k] = {'img': snapshot_img, 'objects': snapshot_data[k]}
+        
+        
+            
+        
     
     
     
--- utils.py
+++ utils.py
@@ -7,3 +7,7 @@ def iou_bbox(pred_bbox, tar_bbox, threshold = 0.1):
     # 2. if iou > threshold, return True, else return False
     return ...
 
+
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
